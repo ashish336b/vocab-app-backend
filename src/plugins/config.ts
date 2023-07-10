95133eefd6a00d7e8cf32f9c2b99b9de2c228078
ashish336b <ashish336b@gmail.com>
2023-07-11 01:02:27 +0545

    Added dotenv expand to use variable in dotenv

--- src/plugins/config.ts
+++ src/plugins/config.ts
@@ -1,8 +1,13 @@
 import 'dotenv/config';
 import fp from 'fastify-plugin';
 import { FastifyPluginAsync } from 'fastify';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 import { Static, Type } from '@sinclair/typebox';
 import Ajv from 'ajv';
+
+const env = dotenv.config();
+dotenvExpand.expand(env);
 
 export enum NodeEnv {
   development = 'development',
